21a0c7af94734314beb0a57a6367b3f969ced560
Saqib Ullah Siddique <saqibullah@gmail.com>
2020-03-03 19:23:08 +0500

    Update in the imagetovideo.py file

--- imagetovideo.py
+++ imagetovideo.py
@@ -1,8 +1,18 @@
+'''
+Author      :   Saqib Ullah Siddiqui
+Date        :   02-March-2020
+Objective   :   Convert Images (.jpg,.jpeg and .png) into Vido file
+
+Usage       :   python imagetovideo.py -i /home/administrator/Documents/dl/images 
+                            -o /home/administrator/Documents/dl/images/video.avi 
+                            -f 20                
+'''
+
+
 import argparse
 import cv2
 import numpy as np
-import glob
-
+import os
 
 
 class ImageToVideo:
@@ -11,15 +21,15 @@ class ImageToVideo:
 
     def generate_frames(self, input, fps):
         frame_array = []
-        for filename in glob.glob(r'/home/administrator/Documents/dl/final-multilabel-v1/images/*.jpg'):
+        for filename in os.listdir(input):
             print(filename)
-            #get each files as array
-            img = cv2.imread(filename,1)
-            height, width, layers = img.shape
-            size = (width,height)
-    
-    		#append image array
-            frame_array.append(img)
+            if filename.endswith(".jpg") or filename.endswith(".jpeg") or filename.endswith(".png"):
+                #create each file name
+                img = cv2.imread(input+"/"+filename,1)
+                height, width, layers = img.shape
+                size = (width,height)    
+                #append image array
+                frame_array.append(img)
 
         out = cv2.VideoWriter(self.output,cv2.VideoWriter_fourcc(*'DIVX'), fps, size)
 
@@ -29,17 +39,17 @@ class ImageToVideo:
         out.release()
 
 
-
-
+# Define Argument Parser for the script
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", required=True)
 ap.add_argument("-o", "--output", required=True)
+ap.add_argument("-f", "--fps", required=True)
 args = vars(ap.parse_args())
 
+# Set user parameters
 input = args["input"]
 output = args["output"]
-
-fps = 0.5
+fps = args["fps"]
 
 # Create ImageToVideo Class object
 image_to_video = ImageToVideo(output)
